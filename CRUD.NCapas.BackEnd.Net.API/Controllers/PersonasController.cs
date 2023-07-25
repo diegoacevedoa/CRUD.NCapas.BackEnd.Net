56282f95d5674af7ef92d78ab160c951b5598135
Diego León Acevedo Arenas <diego.acevedo@proveedores.lineadirecta.com.co>
2023-07-24 23:17:57 -0500

    Se agrega proyecto

---
+++ CRUD.NCapas.BackEnd.Net.API/Controllers/PersonasController.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using CRUD.NCapas.BackEnd.Net.Data.Models;
+using CRUD.NCapas.BackEnd.Net.Domain.To.ViewModels;
+using CRUD.NCapas.BackEnd.Net.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRUD.NCapas.BackEnd.Net.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        private readonly IPersonaService _personaService;
+        private readonly IMapper _mapper;
+
+        public PersonasController(IPersonaService personaService, IMapper mapper)
+        {
+            _personaService = personaService;
+            _mapper = mapper;
+        }
+
+        // GET: api/<PersonasController>
+        [HttpGet]
+        public ActionResult<IEnumerable<PersonaViewModel>> GetAllPersona()
+        {
+            List<Persona> list = _personaService.GetAll();
+            var mapped = _mapper.Map<List<Persona>, List<PersonaViewModel>>(list);
+
+            return mapped;
+        }
+
+        // GET api/<PersonasController>/5
+        [HttpGet("{id}")]
+        public ActionResult<PersonaViewModel> GetByIdPersona(int id)
+        {
+            Persona? data = _personaService.GetById(id);
+            var mapped = _mapper.Map<Persona?, PersonaViewModel>(data);
+
+            return mapped;
+        }
+
+        // POST api/<PersonasController>
+        [HttpPost]
+        public ActionResult<PersonaViewModel> Post([FromBody] Persona model)
+        {
+            Persona data = _personaService.Add(model);
+            var mapped = _mapper.Map<Persona, PersonaViewModel>(data);
+
+            return CreatedAtAction("GetByIdPersona", new { id = mapped.IdPersona }, mapped);
+        }
+
+        // PUT api/<PersonasController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Persona model)
+        {
+            if (id != model.IdPersona)
+            {
+                return BadRequest();
+            }
+
+            _personaService.Update(model);
+
+            return NoContent();
+        }
+
+        // DELETE api/<PersonasController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personaService.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ CRUD.NCapas.BackEnd.Net.Data/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.NCapas.BackEnd.Net.Data.Models
+{
+    public class Persona
+    {
+        [Key]
+        public int IdPersona { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string NoDocumento { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombres { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Apellidos { get; set; }
+    }
+}
